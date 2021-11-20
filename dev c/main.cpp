bbf9dce6e3220a25ba0748f1145d3657ca0ddc66
Manify <manifyg@gmail.com>
2021-11-20 20:34:27 +0500

    Initial commit

---
+++ dev c/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+void string1(std::string string2){
+  std::string on_off_attempt;
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+  std::cin >> on_off_attempt;
+}
+int main() {
+  string string3,string4,string5;
+  // Conduct IT support
+  string(string3);
+  
+  // Check in with Jenn
+  std::cout << "Oh hi Jen!\n";
+  
+  // Conduct IT support again...
+  sstring(string4);
+
+  // Check in with Roy
+  std::cout << "You stole the stress machine? But that's stealing!\n";
+  
+  // Conduct IT support yet again...zzzz...
+  string(string5);
+  
+}
